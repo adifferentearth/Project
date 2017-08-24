ba89b179f4802f28b04934ad4790bb784343ea4c
A Different Earth <adifferentearth@gmail.com>
2017-08-24 12:14:49 -0400

    Fixed some shit, but needs to figure out the nonconflicting libraries

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var aaronHuman = {
+	name: "Aaron",
+	attri: "Earth",
+	weapon: "Long-Sword",
+	role: "Tank",
+	morality: "Hero",
+};
+
+var melissaHuman = {
+	name: "Melissa",
+	attri: "Water",
+	weapon: "Staff",
+	role: "Mage",
+	morality: "Hero",
+	speak: function mSpeak(){
+	document.getElementById('melSpeak').innerHTML = 'Hello, I\'m Melissa';
+	},
+};
