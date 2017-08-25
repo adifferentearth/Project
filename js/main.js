94c4c4152711d212f8ed4d07a6189cea543147cf
A Different Earth <adifferentearth@gmail.com>
2017-08-24 22:54:42 -0400

    YES! Got the functionality embeded in my mind!

--- js/main.js
+++ js/main.js
@@ -14,3 +14,22 @@ var melissaHuman = {
 	morality: "Hero",
 };
 
+// 2. "Name" Functionality ():
+// Make the input receive and store 
+// the values inserted and pass it in to the body at the bottom.
+// connect to 'your-name' and 'final-head'
+
+// 
+function nameInput(){
+	var yourName = document.getElementById('userInput');
+	document.getElementById('yourName').innerHTML = "My name is " + userInput.value + ".";
+	document.getElementById('final-head').innerHTML = userInput.value;
+};
+
+// Attributes Functionalities 
+function earthInput(){
+	var yourAttri = document.getElementById('earthInputFor');
+	document.getElementById('selected-attri').innerHTML = "My Attribute is " + earthInputFor.value + ".";
+	document.getElementById('final-body').innerHTML = "My Attribute is " + yourAttri.value + ".";
+};
+
